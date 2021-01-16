8ee1fd57ca5233e7d7c539c83488731effb2c921
Song <quanpengsong@gmail.com>
2021-01-16 13:59:50 +0800

    :construction: able to retrive data.json now

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"os"
+	"path"
 	"strings"
 )
-
+var dataDir = ".emoji-cli"
 var jsonPath = "./data.json"
 var databasePath = "./data.db"
 
 func initialize()  {
+	home, _ := os.UserHomeDir()
+	dataDir = path.Join(home, dataDir)
+	if _, err := os.Stat(dataDir); os.IsNotExist(err) {
+		_ = os.Mkdir(dataDir, 0777)
+	}
+	jsonPath = path.Join(dataDir, jsonPath)
 	if _, err := os.Stat(jsonPath); os.IsNotExist(err) {
 		fetchData()
 	}
+	databasePath = path.Join(dataDir, databasePath)
 	if _, err := os.Stat(databasePath); os.IsNotExist(err) {
 		updateDatabase()
 	}
--- utils.go
+++ utils.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 )
 
 func printHelpInfo()  {
 	fmt.Println("Help information:")
 	fmt.Println(`1. emoji description: search by description.
 2. emoji -h: print help information.
-3. emoji -u: update local database.`)
+3. emoji -u: update local database.
+
+Notice: database is saved at ` + databasePath)
 }
 
 func updateData()  {
@@ -19,6 +24,20 @@ func updateData()  {
 
 func fetchData()  {
 	log.Print("Fetching data from Github.")
+	url := "https://raw.githubusercontent.com/songquanpeng/emoji-cli/main/data.json"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer resp.Body.Close()
+	out, err := os.Create(jsonPath)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer out.Close()
+	_, err = io.Copy(out, resp.Body)
 	log.Println("Data fetched.")
 }
 
