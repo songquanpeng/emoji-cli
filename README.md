0128efffd6da50f36bd23a581fcccd515e2aba92
Song <quanpengsong@gmail.com>
2021-01-17 21:12:39 +0800

    :construction: save work

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 1. Search emojis in your terminal.
 2. Query github emoji shortcode for your git commit easily.
 
+## Demo
+```
+PS D:\Project\Go\emoji> .\emoji-cli.exe 文档
+📝 :memo:
+📖 :book:
+✏️ :pencil2:
+PS D:\Project\Go\emoji>
+```
+
 ## Installation
 1. Download the executable file.
 2. Add it to a path that in your PATH environment variable.
@@ -12,3 +21,8 @@
 1. emoji description: search by description.
 2. emoji -h: print help information.
 3. emoji -u: update local database.
+
+## Reference
+1. [github/git-commit-emoji-cn](https://github.com/liuchengxu/git-commit-emoji-cn)
+2. [gist/7360908](https://gist.github.com/rxaviers/7360908)
+3. [github/emoji-cheat-sheet](https://github.com/ikatyang/emoji-cheat-sheet/blob/master/README.md)
--- utils.go
+++ utils.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -13,9 +12,7 @@ func printHelpInfo() {
 	fmt.Println("Help information:")
 	fmt.Println(`1. emoji description: search by description.
 2. emoji -h: print help information.
-3. emoji -u: update local database.
-
-Notice: database is saved at ` + databasePath)
+3. emoji -u: update local database.`)
 }
 
 func updateData() {
@@ -24,34 +21,43 @@ func updateData() {
 }
 
 func fetchData() {
-	log.Print("Fetching data from Github.")
+	fmt.Println("Fetching data from Github...")
 	url := "https://raw.githubusercontent.com/songquanpeng/emoji-cli/main/data.json"
+	backupUrl := "https://gitee.com/songquanpeng/emoji-cli/raw/main/data.json"
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
-		return
+		fmt.Println(err)
+		fmt.Println("Failed to fetch data from Github.")
+		fmt.Println("Fetching data from Gitee...")
+		resp, err = http.Get(backupUrl)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Failed to fetch data from Gitee.")
+			fmt.Println("Abort.")
+			return
+		}
 	}
 	defer resp.Body.Close()
 	out, err := os.Create(jsonPath)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 		return
 	}
 	defer out.Close()
 	_, err = io.Copy(out, resp.Body)
-	log.Println("Data fetched.")
+	fmt.Println("Data fetched.")
 }
 
 func updateDatabase() {
-	log.Print("Updating local database.")
+	fmt.Println("Updating local database...")
 	UpdateDatabase()
-	log.Println("Database updated.")
+	fmt.Println("Done.")
 }
 
 func processQuery(description string) {
 	keywords := strings.Split(description, " ")
 	emojis := Query(keywords[0])
-	for i, emoji := range emojis {
-		fmt.Printf("%d. %s :%s:\n", i+1, emoji.Emoji, emoji.Shortcode)
+	for _, emoji := range emojis {
+		fmt.Printf("%s :%s:\n", emoji.Emoji, emoji.Shortcode)
 	}
 }
